761d49c5c8960ad63498304428befb6d479cecc4
10974-spec <emmanuelnyakoe13@gmail.com>
2025-07-28 14:25:33 +0300

    heading

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -9,7 +9,12 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main>
-
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <section className="main-section">
+      <div className="page-heading">
+        <h1 className="">Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedBack.</h2>
+      </div>
+    </section>
   </main>
 }
