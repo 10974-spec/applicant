bbcad55493745d2ed70550c45801a8d254c54113
10974-spec <emmanuelnyakoe13@gmail.com>
2025-07-30 14:47:22 +0300

    resume card

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 
 
@@ -10,11 +11,17 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    
+    <Navbar/>
     <section className="main-section">
       <div className="page-heading">
         <h1 className="">Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedBack.</h2>
       </div>
     </section>
+
+    {[
+ 
+    ]}
   </main>
 }
