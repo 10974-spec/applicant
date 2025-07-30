e9e41f8fb6fa5ee5e8505d963936fecea1da57ce
10974-spec <emmanuelnyakoe13@gmail.com>
2025-07-30 16:48:56 +0300

    resume cards

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -19,15 +19,16 @@ export default function Home() {
           <h1 className="">Track Your Applications & Resume Ratings</h1>
           <h2>Review your submissions and check AI-powered feedBack.</h2>
         </div>
-      </section>
+      
 
       {resumes.length > 0 && (
-        <div className="resume-section">
+        <div className="resumes-section">
           {resumes.map((resume) => (
             <ResumeCard key={resume.id} resume={resume} />
           ))}
         </div>
       )}
+      </section>
     </main>
   );
 }
