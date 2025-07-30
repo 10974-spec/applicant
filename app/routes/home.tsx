d3a19f327bc11d5ff5d6f6f3c169c575b558ea10
10974-spec <emmanuelnyakoe13@gmail.com>
2025-07-30 15:29:10 +0300

    score circle

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-
+import { resumes } from "~/constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,18 +11,23 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-    
-    <Navbar/>
-    <section className="main-section">
-      <div className="page-heading">
-        <h1 className="">Track Your Applications & Resume Ratings</h1>
-        <h2>Review your submissions and check AI-powered feedBack.</h2>
-      </div>
-    </section>
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1 className="">Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedBack.</h2>
+        </div>
+      </section>
 
-    {[
- 
-    ]}
-  </main>
+      {resumes.length > 0 && (
+        <div className="resume-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
